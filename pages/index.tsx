bd7db25c2c39c901ef9be1bab71d94344bfe93a2
Caio Silva <caiossantosp5560@gmail.com>
2023-06-23 10:09:13 -0300

    feat: criar header da page HomeNoAuth - Header não Autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from "next/head";
 import styles from "@/styles/HomeNoAuth.module.scss"
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>OneBitFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="OneFitFlix" key="title"/>
+        <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma
+          forma simples e fácil!"/>
       </Head>
       <main>
+        <HeaderNoAuth/>
       </main>
     </>
   )
