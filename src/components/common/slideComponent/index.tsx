c9f5d38b5c01831a2f0f1d80e9c7df6b4da2bc66
Caio Silva <caiossantosp5560@gmail.com>
2023-06-24 18:21:51 -0300

    feat: criar slides e conexão com o backend

---
+++ src/components/common/slideComponent/index.tsx
@@ -0,0 +1,31 @@
+import { CourseType } from "@/src/services/courseService";
+import SlideCard from "../slideCard";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+interface props {
+  course: CourseType[]
+}
+
+const SlideComponent = ({ course }:props) => {
+  return (
+    <>
+      <div>
+        <Splide options={{ 
+          type: "loop",
+          perPage: 4,
+          perMove: 1,
+          pagination: false
+        }}>
+         {course?.map((course) => (
+          <SplideSlide key={course.id}>
+            <SlideCard course={course}/>
+          </SplideSlide>
+         ))}
+        </Splide>
+      </div>
+    </>
+  )
+}
+
+export default SlideComponent;
