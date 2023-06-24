7675747daf32298a2bf052f49085a3b8c4541ab6
Caio Silva <caiossantosp5560@gmail.com>
2023-06-24 18:48:25 -0300

    feat: receber resposta do backend nos slides

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,16 @@ import styles from "@/styles/HomeNoAuth.module.scss"
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
 import CardSection from "@/src/components/homeNoAuth/cardsSection";
+import SlideSection from "@/src/components/homeNoAuth/slideSection";
+import { GetStaticProps } from "next";
+import courseService, { CourseType } from "@/src/services/courseService";
 
-const HomeNoAuth = () => {
+interface indexPageProps {
+  children ?: React.ReactNode;
+  course: CourseType[]
+}
+
+const HomeNoAuth = ({ course }: indexPageProps) => {
   return (
     <>
       <Head>
@@ -20,9 +28,21 @@ const HomeNoAuth = () => {
           <PresentationSection />
         </div>
         <CardSection />
+        <SlideSection newestCourses={course} />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+  const res = await courseService.getNewestCourses();
+
+  return {
+    props: {
+      course: res.data
+    },
+    revalidate: 3600 * 24,
+  }
 }
 
 export default HomeNoAuth;
