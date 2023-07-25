55bf4c82143015bfbdbd063edf5d50f04fb92883
Caio Silva <caiossantosp5560@gmail.com>
2023-07-25 17:13:27 -0300

    feat: adicionar estilos na página de search

--- pages/search.tsx
+++ pages/search.tsx
@@ -3,8 +3,11 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import HeaderAuth from "@/src/components/common/headerAuth";
 import courseService, { CourseType } from "@/src/services/courseService";
+import SearchCard from "@/src/components/searchCard";
 
 import styles from "../styles/search.module.scss";
+import { Container } from "reactstrap";
+import Footer from "@/src/components/common/footer";
 
 const Search = () => {
   const [searchResult, setSearchResult] = useState<CourseType[]>([]);
@@ -29,13 +32,26 @@ const Search = () => {
         <title>Onebitflix - {searchName}</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
-      <main>
-        <HeaderAuth />
-        {searchResult?.map((course) => (
-          <div key={course?.id}>
-            <p>{course?.name}</p>
+      <main className={styles.main}>
+        <div className={styles.headerFooterBg}>
+          <HeaderAuth />
+        </div>
+        {searchResult?.length >= 1 ? (
+          <div className={styles.searchResult}>
+            <Container className="d-flex flex-wrap justify-content-center gap-5 py-">
+              {searchResult?.map((course) => (
+                <SearchCard key={course?.id} course={course}/>))
+              }
+            </Container>
           </div>
-        ))}
+        ) : (
+          <>
+            <p className={styles.noSearchResult}>Nenhum resultado encontrado</p>
+          </>
+        )}
+        <div className={styles.headerFooterBg}>
+          <Footer />
+        </div>
       </main>
     </>
   )
